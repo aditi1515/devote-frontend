696ff02c81a0c442a275424fe65cd7026023b3ab
Jatin Pandey <jatin26pandey@gmail.com>
2023-12-18 09:58:38 +0530

    signup fix

--- src/components/SignupForm/SignUp.jsx
+++ src/components/SignupForm/SignUp.jsx
@@ -1,9 +1,44 @@
-import React from "react";
-import "./signup.scss"
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { URL } from "../../config/data";
+import { login } from "../../config/userReducer";
+import "./signup.scss";
 export const SignUp = () => {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+
+  const [formdata, setFormData] = useState({ houseaddress: "30 Wellington Square" })
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const handleChange = (e) => {
+    setFormData({ ...formdata, [e.target.id]: e.target.value })
+  }
+
+  async function handleSignIn() {
+    console.log(formdata);
+    try {
+      const response = await axios.post(
+        `${URL}/user/signup`,
+        formdata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
+      console.log(response?.data);
+      navigate("/login")
+    } catch (error) {
+      console.log(error);
+    }
+    finally {
+    }
+
+
+  }
+
   return (
     <div className="signup-main-container">
       <div className="signup-imageContainer">
@@ -14,27 +49,27 @@ export const SignUp = () => {
         <div className="signup-fields">
           {" "}
           <h3 className="signup-label">First Name</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="firstname" className="signup-inputs" />
           <h3 className="signup-label">Last Name</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="lastname" className="signup-inputs" />
           <h3 className="signup-label">Age</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="age" className="signup-inputs" />
           <h3 className="signup-label">Password</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="password" onChange={handleChange} id="password" className="signup-inputs" />
           <h3 className="signup-label">Gender</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="gender" className="signup-inputs" />
           <h3 className="signup-label">Email</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="email" className="signup-inputs" />
           <h3 className="signup-label">Aadhaar Number</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="aadhaarNumber" className="signup-inputs" />
           <h3 className="signup-label">Upload your image</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="imageUrl" className="signup-inputs" />
           <h3 className="signup-label">Voter Constituency</h3>
-          <input type="text" className="signup-inputs" />
+          <input type="text" onChange={handleChange} id="voterConstituency" className="signup-inputs" />
         </div>
         <div className="signup-forgotPass">
           {" "}
-          <button className="signup-btn" >Sign In</button>
+          <button className="signup-btn" onClick={handleSignIn}>Sign In</button>
         </div>
         <p className="signUp">
           Already registered? <span className="signup-Link"><Link className="link" to={"/login"}>Login</Link></span>
--- src/config/data.js
+++ src/config/data.js
@@ -1,28 +1,30 @@
-export const URL = "https://de-vote-api.vercel.app"
+// export const URL = "https://de-vote-api.vercel.app"
+export const URL = "http://localhost:3000";
+
 export const VoteData = [
-  {
-    id: 1,
-    partyName: "A",
-    votes: 200,
-  },
-  {
-    id: 2,
-    partyName: "B",
-    votes: 50,
-  },
-  {
-    id: 3,
-    partyName: "C",
-    votes: 800,
-  },
-  {
-    id: 4,
-    partyName: "D",
-    votes: 500,
-  },
-  {
-    id: 5,
-    partyName: "E",
-    votes: 300,
-  },
+ {
+  id: 1,
+  partyName: "A",
+  votes: 200,
+ },
+ {
+  id: 2,
+  partyName: "B",
+  votes: 50,
+ },
+ {
+  id: 3,
+  partyName: "C",
+  votes: 800,
+ },
+ {
+  id: 4,
+  partyName: "D",
+  votes: 500,
+ },
+ {
+  id: 5,
+  partyName: "E",
+  votes: 300,
+ },
 ];
